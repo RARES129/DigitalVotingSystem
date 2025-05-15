c9db6e97c2aff1b005fd5e28cc165db0fc4a8ab9
Dascălu Rareș <rares-vasilica.dascalu@s.unibuc.ro>
2025-05-15 16:33:13 +0300

    Database setup

--- src/main/java/com/digitalvotingsystem/demo/DVSApplication.java
+++ src/main/java/com/digitalvotingsystem/demo/DVSApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DVSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DVSApplication.class, args);
 	}
 
 }
